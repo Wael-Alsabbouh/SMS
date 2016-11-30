4eeeb6ac43fb7125df2ea188c97c532ab8579193
Mohammad <md_ibra@hotmail.com>
2016-11-30 14:52:23 +0100

    ejs

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-/*====================      The redirecting file to index.ejs,     50% Temporary page,    In the future this page could be  deleted   ==============*/
-/*
- * GET home page.
- */
-
-exports.index = function(req, res){
-  res.render('index', { title: 'Great TEAM' });
-};
